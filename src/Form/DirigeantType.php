5707deca5f8d35da4fac76a128e579135a3ed6d3
Onja <randrianasoloonjamy@gmail.com>
2021-10-08 15:40:14 +0300

    update maj2

--- src/Form/DirigeantType.php
+++ src/Form/DirigeantType.php
@@ -6,6 +6,7 @@ use App\Entity\Dirigeants;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class DirigeantType extends AbstractType
@@ -13,9 +14,18 @@ class DirigeantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
+            ->add('nom',TextType::class,[
+                'attr'=>[
+                    'placeholder'=>'Nom'
+                ]
+            ])
+            ->add('prenom',TextType::class,[
+                'attr'=>[
+                    'placeholder'=>'Prénom'
+                ]
+            ])
             ->add('sexe', ChoiceType::class,[
+
                 'choices' => [
                     'Homme' => 'Homme',
                     'Femme' => 'Femme'
@@ -24,7 +34,11 @@ class DirigeantType extends AbstractType
                 'expanded'=>true,
                 'multiple'=>false
             ])
-            ->add('mail')
+            ->add('mail',TextType::class,[
+                'attr'=>[
+                    'placeholder'=>'ex : nom@gmail.com'
+                ]
+            ])
         ;
     }
 
